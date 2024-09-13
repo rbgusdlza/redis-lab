b50e2e472730d37e78fd0dc2f940bebc66b13f30
rbgusdlza <rbgusdlza@naver.com>
2024-09-14 01:54:41 +0900

    init

---
+++ src/test/java/rbgusdlza/redislab/RedislabApplicationTests.java
@@ -0,0 +1,13 @@
+package rbgusdlza.redislab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedislabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
